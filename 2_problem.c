7dd27ff715d35c871058bd22c9537751d17b2a54
raven-une <gautam.jnk10@gmail.com>
2025-08-30 20:03:48 +0545

    first git commit

---
+++ 2_problem.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main ()
+{
+    int a =5;
+    printf("The value of a is %d",a);
+return 0;
+}
---
+++ first.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+    printf("hello this is first use of git ");
+    return 0;
+}
