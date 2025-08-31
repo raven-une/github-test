23cf6110a31ec65b8f808b2202ae36d6c18e077b
raven-une <gautam.jnk10@gmail.com>
2025-08-31 14:49:19 +0545

    add if-rlse

---
+++ if-else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int age = 16;
+    if(age<20)
+    {
+        printf("You cannot drive ");
+    }
+    else {
+        printf("you can drive!!");
+    }
+    return 0;
+}
