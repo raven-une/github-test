ba1cf04f4a3749fbaae7bc6ab588780c386b8e0a
raven-une <gautam.jnk10@gmail.com>
2025-08-31 12:09:16 +0545

    add mul

--- fix.c
+++ fix.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 int fix(int , int );
+
+int sub(int a , int b)
+{
+    printf("the multiply is %d\n",a*b);
+    return 0;
+}
+
 int main()
 {
     printf("This is a bug fix file\n");
-    printf("the return value is %d",fix(4,5));
+    printf("the return value is %d\n",fix(4,5));
+    sub(2,4);
     return 0;
 }
 
 int fix(int a , int b){
     return a + b;
 }
+
