667907a4fa4a51a1eb5657a3a833c79f040645de
raven-une <gautam.jnk10@gmail.com>
2025-08-31 12:06:11 +0545

    add sub

--- fix.c
+++ fix.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 int fix(int , int );
+int sub(int a , int b);
+
 int main()
 {
     printf("This is a bug fix file\n");
-    printf("the return value is %d",fix(4,5));
+    printf("the return value is %d\n",fix(4,5));
+    printf("the sub is %d",sub(7,4));
     return 0;
 }
 
 int fix(int a , int b){
     return a + b;
+}
+
+int sub(int a , int b)
+{
+    return a - b;
 }
