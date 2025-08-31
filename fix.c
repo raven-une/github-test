bd621301b3bd2e4a3691888c2ba2bf5966acb4fe
raven-une <gautam.jnk10@gmail.com>
2025-08-31 12:15:46 +0545

    change func name to add

--- fix.c
+++ fix.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int fix(int , int );
+int add(int , int );
 int sub(int a , int b);
 
 int main()
 {
     printf("This is a bug fix file\n");
-    printf("the return value is %d\n",fix(4,5));
+    printf("the  value of add is %d\n",fix(4,5));
     printf("the sub is %d",sub(7,4));
     return 0;
 }
 
-int fix(int a , int b){
+int add(int a , int b){
     return a + b;
 }
 
